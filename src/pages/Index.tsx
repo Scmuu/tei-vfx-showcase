eb9ecb95c856dc4d832909aab059246f0fc5d6b9
Scmuu <la.tua.email@gmail.com>
2026-03-29 23:26:00 +0200

    asset aggiunto

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import PricingSection from "@/components/PricingSection";
 import SocialsSection from "@/components/SocialsSection";
 import CursorTrail from "@/components/CursorTrail";
 import centipedeImg from "@/assets/centipede.png";
+import kuraiImg from "@/assets/Kurai2d.png";
 
 const Index = () => {
   return (
@@ -12,16 +13,28 @@ const Index = () => {
       <CursorTrail />
       <Navbar />
 
-      {/* Centipede image on the right side */}
+      {/* Centipede DESTRA */}
       <img
         src={centipedeImg}
-        alt=""
+        alt="Centipede"
         className="fixed right-0 top-0 h-full w-auto object-cover pointer-events-none z-[5] opacity-100"
         style={{ maxWidth: "350px" }}
       />
 
       <div className="relative z-10">
         <HeroSection />
+        
+        {/* KURAI CENTRATA SOTTO */}
+        <section className="w-full py-20 bg-gradient-to-b from-background to-muted/30">
+          <div className="container mx-auto px-6 max-w-5xl">
+            <img 
+              src={kuraiImg} 
+              alt="Kurai TEI VFX Rocket League" 
+              className="w-full h-auto rounded-3xl shadow-2xl mx-auto block object-contain max-h-[500px]"
+            />
+          </div>
+        </section>
+        
         <ServicesSection />
         <PricingSection />
         <SocialsSection />
